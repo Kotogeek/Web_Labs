947aab34d2e5dd6dae0be65b0e8020b4462a45b8
Kotogeek <91885899+Kotogeek@users.noreply.github.com>
2023-01-18 13:00:25 +0300

    Create delete.php

---
+++ Lab 4/delete.php
@@ -0,0 +1,24 @@
+<?php 
+    // 
+    if (isset($_GET['id'])) {
+
+        // id из запроса
+        $id = $_GET['id'];
+
+        $xml = simplexml_load_file("data.xml");
+
+        $i = 0; 
+
+
+        foreach($xml->post as $post) {
+            if ($post['id'] == $id) {
+                unset($xml->post[$i]);
+                break;
+            }
+            $i++;
+        }
+
+        $xml->saveXML("data.xml");
+        echo "<script>location.href='index.php'</script>";
+    }
+?>
